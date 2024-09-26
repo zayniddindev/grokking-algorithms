ee432e3aec86abd586829a3e4ab6915de44e937a
Zayniddin Otabaev <zayniddindev@gmail.com>
2024-09-26 09:34:26 +0500

    initial commit: binary search

---
+++ binary-search.js
@@ -0,0 +1,31 @@
+const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const wantedNumber = 21;
+
+/**
+ * @param {T[]} sortedArray
+ * @param {T} wantedNumber
+ * @returns {number|null}
+ */
+const binarySearch = (sortedArray, wantedNumber) => {
+  let low = 0;
+  let high = sortedArray.length - 1;
+  while (low <= high) {
+    const mid = Math.floor(low + high);
+    const guess = sortedArray[mid];
+    if (guess == wantedNumber) {
+      return mid;
+    }
+    if (guess > wantedNumber) {
+      high = mid - 1;
+      continue;
+    }
+    if (guess < wantedNumber) {
+      low = mid + 1;
+      continue;
+    }
+  }
+  return null;
+};
+
+const index = binarySearch(sortedArray, wantedNumber);
+console.log(index);
